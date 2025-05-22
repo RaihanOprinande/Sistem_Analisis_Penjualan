2fb2212df10d0b354d375b3bf2e85d4c7cc979cd
RaihanOprinande <opri.fahdi@gmail.com>
2025-05-22 10:05:49 +0700

    done login form

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="flex min-h-screen">
+        <div class="w-64">
+            @include('layouts.sidebar')
+        </div>
+        <div class="flex-1 container pt-16 ms-10 mt-10 me-10">
+            @yield('content')
+        </div>
+    </div>
+    @include('layouts.footer')
+
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+Route::get('/dashboard',[DashboardController::class,'index']);
+Route::get('/register',[RegisterController::class,'index']);
+Route::get('/login',[LoginController::class,'index']);
+
+Route::post('/login',[LoginController::class,'authenticate']);
+Route::post('/register',[RegisterController::class,'store']);
+Route::post('/logout',[LoginController::class,'logout']);
